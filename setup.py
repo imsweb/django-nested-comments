7176a86c1e8c13f12e5a347f2877a1cf4c62505d
lefcourn <lefcourn@imsweb.com>
2017-02-08 16:54:52 -0500

    Corrected mptt package name

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'mptt',
+        'django-mptt',
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
