231a33d4e9c3af38a07e4ee5bdb4512943ccd457
Brad Rutten <RuttenB@imsweb.com>
2019-01-03 08:42:09 -0500

    Updated the comment_changed singal so the Comment class is the sender. Added a comment to comments.html explaining what "last-node-container" is and how it's calculated

--- comments/signals.py
+++ comments/signals.py
@@ -2,4 +2,4 @@ from django.dispatch import Signal
 
 # The 'sender' will be the comment that changed
 # 'kwargs' will contain the dictionary passed back from the ajax call in the 'X_KWARGS' header
-comment_changed = Signal(providing_args=('request', 'version_saved', 'comment_action', 'kwargs'))
+comment_changed = Signal(providing_args=('comment', 'request', 'version_saved', 'comment_action', 'kwargs'))
