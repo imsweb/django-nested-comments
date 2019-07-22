4b50af1fceda9d4bc2840f935491cbd66a2a8b63
Bradly Rutten <ruttenb@imsweb.com>
2019-07-22 10:26:42 -0400

    Updated Development Status in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
         'bleach',
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
