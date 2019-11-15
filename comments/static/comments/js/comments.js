a0e9532084438ca61dc9e11d1dbfef2ab483b72e
salsburj <SalsburyJ@imsweb.com>
2019-11-15 17:02:52 -0500

    1953 - remove validation in here
    instead do it in bioshare's save_comment

--- comments/static/comments/js/comments.js
+++ comments/static/comments/js/comments.js
@@ -154,11 +154,7 @@
                         message_holder.val(commentForm.find('input[name=message]').val());
                         settings.handlePostError(settings, response);
                     });
-                    var commentData = settings.getData(settings, commentForm);
-                    var comment = commentData[1].value;
-                    if (comment !== "" || $('.upload-item').length !== 0) {
-                        settings.post_data(settings.postUrl, commentData, callback);
-                    }
+                    settings.post_data(settings.postUrl, commentData, callback);
                     break;
                 case 'post-edit':
                     var commentForm = $(this).closest(settings.commentFormSelector);
