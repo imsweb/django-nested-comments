9f24c46906c27c9af55a4383c641df80ebd1face
Jiang, Darren (IMS) <jiangd@imsweb.com>
2024-03-08 13:14:05 -0500

    remove attribute whitespace

--- comments/forms.py
+++ comments/forms.py
@@ -12,6 +12,6 @@ class CommentVersionForm(forms.ModelForm):
         
     def clean_message(self):
         message = self.cleaned_data['message']
-        allowed_tags = settings.ALLOWED_TAGS if hasattr(settings, 'ALLOWED_TAGS') else settings.BLEACH_ALLOWED_TAGS if hasattr(settings, 'BLEACH_ALLOWED_TAGS ') else bleach.ALLOWED_TAGS
-        allowed_attributes = settings.ALLOWED_ATTRIBUTES if hasattr(settings, 'ALLOWED_ATTRIBUTES') else settings.BLEACH_ALLOWED_ATTRIBUTES if hasattr(settings, 'BLEACH_ALLOWED_ATTRIBUTES ') else bleach.ALLOWED_ATTRIBUTES
+        allowed_tags = settings.ALLOWED_TAGS if hasattr(settings, 'ALLOWED_TAGS') else settings.BLEACH_ALLOWED_TAGS if hasattr(settings, 'BLEACH_ALLOWED_TAGS') else bleach.ALLOWED_TAGS
+        allowed_attributes = settings.ALLOWED_ATTRIBUTES if hasattr(settings, 'ALLOWED_ATTRIBUTES') else settings.BLEACH_ALLOWED_ATTRIBUTES if hasattr(settings, 'BLEACH_ALLOWED_ATTRIBUTES') else bleach.ALLOWED_ATTRIBUTES
         return bleach.clean(message, tags=allowed_tags, attributes=allowed_attributes, strip=True)
